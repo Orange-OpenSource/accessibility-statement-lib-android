76ea07636c24c8a2a35c18706c0857353a82673b
Florent Maitre <florent.maitre@orange.com>
2023-11-09 14:13:10 +0100

    Add support to display accessibility statement from assets

--- AccessibilityStatementLibrary/src/main/java/com/orange/accessibilitystatementlibrary/StatementView.kt
+++ AccessibilityStatementLibrary/src/main/java/com/orange/accessibilitystatementlibrary/StatementView.kt
@@ -24,6 +24,7 @@
 
 package com.orange.accessibilitystatementlibrary
 
+import android.content.ClipData
 import android.content.Context
 import android.content.Intent
 import android.content.res.TypedArray
@@ -31,7 +32,12 @@ import android.net.Uri
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.FileProvider
 import com.orange.accessibilitystatementlibrary.databinding.ViewStatementBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.io.File
 
 class StatementView @JvmOverloads constructor(
     context: Context,
@@ -40,18 +46,34 @@ class StatementView @JvmOverloads constructor(
 ) :
     ConstraintLayout(context, attrs, defStyleAttr) {
 
-    var urlAccessibilityDeclaration: String? = null
-    lateinit var binding : ViewStatementBinding
+    companion object {
 
-    init {
-        init(attrs)
+        private const val ACCESSIBILITY_STATEMENT_ASSETS_DIRECTORY = "accessibility-statement"
     }
 
-    private fun init(attrs: AttributeSet?) {
+    var urlAccessibilityDeclaration: String? = null
+
+    var accessibilityStatementFilePath = "accessibility_result.xml"
+        set(value) {
+            field = value
+            displayResults()
+        }
+
+    val binding: ViewStatementBinding
+
+    private val coroutineScope = CoroutineScope(Dispatchers.IO)
+
+    private var isAssetsCopyFinished = false
+
+    init {
         binding = ViewStatementBinding.inflate(LayoutInflater.from(context), this)
         val attributes = context.obtainStyledAttributes(attrs, R.styleable.DeclarationView)
+        attributes.getString(R.styleable.DeclarationView_file_path)?.let { accessibilityStatementFilePath = it }
         displayResults()
         initMoreDetailsButton(attributes)
+        coroutineScope.launch {
+            copyAssetsToFilesDirectory(context)
+        }
     }
 
     private fun initMoreDetailsButton(attributes: TypedArray) {
@@ -62,11 +84,12 @@ class StatementView @JvmOverloads constructor(
             binding.declarantTextView.text = text
 
             binding.buttonSeeMore.setOnClickListener {
-                if (urlAccessibilityDeclaration != null) {
-                    val browserIntent =
-                        Intent(Intent.ACTION_VIEW, Uri.parse(urlAccessibilityDeclaration))
-                    context.startActivity(browserIntent)
+                val intent = if (urlAccessibilityDeclaration != null) {
+                    Intent(Intent.ACTION_VIEW, Uri.parse(urlAccessibilityDeclaration))
+                } else {
+                    getAccessibilityStatementAssetsIntent(context)
                 }
+                intent?.let { context.startActivity(intent) }
             }
         } finally {
             attributes.recycle()
@@ -74,10 +97,10 @@ class StatementView @JvmOverloads constructor(
     }
 
     private fun displayResults() {
-        val statementXMLParser = StatementXMLParser(context)
+        val statementXMLParser = StatementXMLParser(context, accessibilityStatementFilePath)
         val accessibilityStatement = statementXMLParser.getAccessibilityStatementFromXML()
 
-        val resultPercentValue = accessibilityStatement.resultScore?.toInt() ?: 0
+        val resultPercentValue = accessibilityStatement?.resultScore?.toInt() ?: 0
         binding.resultTextView.text = context.getString(R.string.result, resultPercentValue)
         binding.resultTextView.contentDescription =
             context.getString(R.string.result_content_desc, resultPercentValue)
@@ -85,11 +108,11 @@ class StatementView @JvmOverloads constructor(
         binding.resultProgresBar.max = 100
         binding.resultProgresBar.progress = resultPercentValue
 
-        binding.dateTextView.text = accessibilityStatement.getDateddMMyyyyFormat()
-        binding.referentialTextView.text = accessibilityStatement.referential
-        binding.technologieTextView.text = accessibilityStatement.technologies
+        binding.dateTextView.text = accessibilityStatement?.getDateddMMyyyyFormat()
+        binding.referentialTextView.text = accessibilityStatement?.referential
+        binding.technologieTextView.text = accessibilityStatement?.technologies
 
-        displayComplianceStatus(resultPercentValue, accessibilityStatement.title)
+        displayComplianceStatus(resultPercentValue, accessibilityStatement?.title)
     }
 
     private fun displayComplianceStatus(percentValue: Int, title: String?) {
@@ -100,5 +123,68 @@ class StatementView @JvmOverloads constructor(
         binding.resultDetailTextView.text = context.getString(R.string.conformity_state, nameOfApplication, percentValue)
     }
 
+    private fun copyAssetsToFilesDirectory(context: Context) {
+        val filesDirPath = context.filesDir.absolutePath
 
+        // Create destination path
+        with(File("$filesDirPath/$ACCESSIBILITY_STATEMENT_ASSETS_DIRECTORY")) {
+            if (!exists()) {
+                mkdirs()
+            }
+        }
+
+        // Copy assets to destination
+        getAccessibilityStatementAssetsPaths(context).forEach { path ->
+            context.assets.open(path).use { inputSteam ->
+                val file = File(filesDirPath, path)
+                if (!file.exists()) {
+                    file.outputStream().use { outputStream ->
+                        inputSteam.copyTo(outputStream, 1024)
+                    }
+                }
+            }
+        }
+
+        isAssetsCopyFinished = true
+    }
+
+    private fun getAccessibilityStatementAssetsIntent(context: Context): Intent? {
+        val uris = getAccessibilityStatementAssetsPaths(context).mapNotNull { path ->
+            val file = File(context.filesDir, path)
+            val authority = "${context.packageName}.accessibilitystatementfileprovider"
+            FileProvider.getUriForFile(context, authority, file)
+        }
+
+        val htmlURI = uris.firstOrNull { it.isHtmlFile }
+        val intent = if (htmlURI != null && isAssetsCopyFinished) {
+            // Add other files (css, images, etc...) to a clip data
+            var clipData: ClipData? = null
+            uris.filter { !it.isHtmlFile }
+                .forEach { uri ->
+                    if (clipData == null) {
+                        clipData = ClipData.newRawUri("", uri)
+                    } else {
+                        clipData?.addItem(ClipData.Item(uri))
+                    }
+                }
+
+            Intent(Intent.ACTION_VIEW, htmlURI).apply {
+                this.clipData = clipData
+                addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+            }
+        } else {
+            null
+        }
+
+        return intent
+    }
+
+    private fun getAccessibilityStatementAssetsPaths(context: Context): List<String> {
+        return with(ACCESSIBILITY_STATEMENT_ASSETS_DIRECTORY) {
+            context.assets.list(this).orEmpty().map { "$this/$it" }
+        }
+    }
+
+    private val Uri.isHtmlFile: Boolean
+        get() = path?.endsWith(".html") == true
 }
